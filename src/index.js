60271ae78c6bcbb6a69680a5bc10cdbc7691c372
an <anvalenton@gmail.com>
2021-05-07 23:08:58 -0400

    updated process env url

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter as Router} from 'react-router-dom';
 
 ReactDOM.render(
+  <Router basename={process.env.PUBLIC_URL}>
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode></Router>,
   document.getElementById('root')
 );
 // ReactDOM.render(<App />,document.getElementById('root'));
